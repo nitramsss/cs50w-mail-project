7bd4dc08936a3f8ce8bfec4f010516ad7386139a
fitzjaymartin_18 <jaymarnitram08@gmail.com>
2025-02-21 16:29:51 +0800

    archive button not finish

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -51,21 +51,22 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   if (mailbox === 'inbox'){
+    // Get all received inbox
     mailBox('inbox');
   } else if (mailbox === 'sent'){
+    // Get all sent inbox
     mailBox('sent');
   } else {
+    // Get all archived inbox
     mailBox('archive');
   }
 
 }
 
 function mailBox(event) {
-
   fetch(`/emails/${event}`)
   .then(response => response.json())
   .then(emails => {
-    
     const emailsView = document.querySelector('#emails-view');  
 
     // if there are no inbox yet
@@ -75,7 +76,6 @@ function mailBox(event) {
 
     // show all email
     emails.forEach(email => {
-
       const div = document.createElement('div');
 
       div.addEventListener('click', () => {
@@ -89,19 +89,24 @@ function mailBox(event) {
       } else {
         div.style.backgroundColor = 'gray';
       }      
-
+      
+      // style the div
       div.style.padding = '10px';
       div.style.borderRadius = '5px';
       div.style.border = '1px solid black';
       div.style.marginBottom = '3px';
 
+      // Show the sender, subject, and time in div
       div.innerHTML = `<h6>${email.sender}</h6><p>${email.subject}</p><p>${email.timestamp}</p>`;
       
+      // Make it responsive
       div.style.display = "flex";
       div.style.flexWrap = "wrap";
       div.style.justifyContent = "space-between";
 
+      // Append to html div
       emailsView.appendChild(div);
+      
     });
 
   })
@@ -110,7 +115,7 @@ function mailBox(event) {
 
 
 function sendMail(toRecipients, toSubject, toBody) {
-    
+  
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
@@ -119,7 +124,7 @@ function sendMail(toRecipients, toSubject, toBody) {
         body: `${toBody}`
     })
   })
-  .then(response => response.json())
+  .then(response => console.log(response.json()))
   .then(result => {
     // Print result
     console.log(result);
@@ -128,7 +133,6 @@ function sendMail(toRecipients, toSubject, toBody) {
 
 
 function showEmail(id) {
-  
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#inbox-view').style.display = 'block';
@@ -137,16 +141,15 @@ function showEmail(id) {
   .then(response => response.json())
   .then(email => {
       // Print email
-      console.log(email);
+      // console.log(email);
 
-      // ... do something else with email ...
-
+      // Get all div to be used
       const fromUser = document.querySelector('#from-user');
       const toUser = document.querySelector('#to-user');
       const emailSubject = document.querySelector('#email-subject');
       const emailBody = document.querySelector('#email-body');
 
-
+      // Style sender box
       fromUser.style.border = '1px solid black';
       fromUser.style.borderRadius = '5px';
       fromUser.style.padding = '10px';
@@ -154,6 +157,7 @@ function showEmail(id) {
       fromUser.style.display = 'flex';
       fromUser.style.flexWrap = 'wrap';
 
+      // Style receiver box
       toUser.style.border = '1px solid black';
       toUser.style.borderRadius = '5px';
       toUser.style.paddingLeft = '10px';
@@ -166,6 +170,7 @@ function showEmail(id) {
       toUser.style.flexDirection = 'column';
       toUser.style.lineHeight  =  '0.5';
 
+      // Style subject box
       emailSubject.style.border = '1px solid black';
       emailSubject.style.borderRadius = '5px';
       emailSubject.style.padding = '10px';
@@ -173,6 +178,7 @@ function showEmail(id) {
       emailSubject.style.display = 'flex';
       emailSubject.style.flexWrap = 'wrap';
 
+      // Style body box
       emailBody.style.border = '1px solid black';
       emailBody.style.borderRadius = '5px';
       emailBody.style.padding = '10px';
@@ -183,6 +189,9 @@ function showEmail(id) {
       // inserting contents
       fromUser.innerHTML = `${email.sender}`;
 
+      toUser.innerHTML = '';
+
+      // Insert each recipients inside the receivers' box
       const recipients = email.recipients;
       recipients.forEach(recipient => {
 
@@ -195,14 +204,40 @@ function showEmail(id) {
       emailSubject.innerHTML = `${email.subject}`;
       emailBody.innerHTML = `${email.body}`;
 
-      // if (email.sender !== )
-      const button = document.createElement('button');
-      const divTarget = document.querySelector('#inbox-button');
 
-      // to do
-      
-      
+      // Archive and unarchive button
+      const currentUser = document.querySelector('[data-current-user]');
 
+      console.log(currentUser.dataset.currentUser)
+      if (email.sender !== currentUser.dataset.currentUser) {
+        const button = document.createElement('button');
+        const divTarget = document.querySelector('#inbox-button');
+
+        divTarget.innerHTML = '';
+
+        button.style.borderRadius = '5px';
+        button.style.backgroundClip = 'white';
+
+        
+        if (email.archive) {
+          button.innerHTML = "Unarchive";
+        } else {
+          button.innerHTML = "Archive";
+        }
+
+        divTarget.appendChild(button);
+
+        button.addEventListener('click', () => {
+
+          if (email.archive) {
+            console.log(email.archive)
+            archiveUnarchive(email.id, 'false');
+          } else {
+            console.log(email.archive)
+            archiveUnarchive(email.id, 'true');
+          }
+        })
+      }
 
   });
 
@@ -216,17 +251,10 @@ function showEmail(id) {
 
 
 function archiveUnarchive(emailId, event) {
-
-
-
-
-  fetch(`/emails/${id}`, {
+  fetch(`/emails/${emailId}`, {
     method: 'PUT',
     body: JSON.stringify({
-        read: true
+      archive: Boolean(`${event}`)
     })
   })
-  // if archive make the button unarchive 
-  // else make the button archive
-
 }
